d87d05b9a4c8e09337be86bceeecf328b738cca3
Espen Sommer Eide <espensommer@gmail.com>
2018-12-24 17:04:54 +0100

    First working version of stops graphics

--- app/assets/javascripts/timeknots.js
+++ app/assets/javascripts/timeknots.js
@@ -124,6 +124,8 @@ var TimeKnots = {
     .data(events).enter()
     .append("circle")
     .attr("class", "timeline-event")
+    .attr("data-argtitle", function(d) { return d.argtitle})
+    .attr("data-arglink", function(d) { return d.arglink})
     .attr("r", function(d){if(d.radius != undefined){return d.radius} return cfg.radius})
     .style("stroke", function(d){
                     if(d.color != undefined){
@@ -154,6 +156,8 @@ var TimeKnots = {
           return x;
         }
         return Math.floor(cfg.width/2)
+    }).on("click", function(d){
+      //alert("click");
     }).on("mouseover", function(d){
       if(cfg.dateDimension){
         var format = d3.timeFormat(cfg.dateFormat);
@@ -164,18 +168,18 @@ var TimeKnots = {
         var datetime = d.value;
         var dateValue = d.name +" <small>("+d.value+")</small>";
       }
+
       d3.select(this)
       .style("fill", function(d){if(d.color != undefined){return d.color} return cfg.color}).transition()
       .duration(100).attr("r",  function(d){if(d.radius != undefined){return Math.floor(d.radius*1.5)} return Math.floor(cfg.radius*1.5)});
-      tip.html("");
+      //tip.html("");
       if(d.img != undefined){
-        tip.append("img").style("float", "left").style("margin-right", "4px").attr("src", d.img).attr("width", "64px");
+      //  tip.append("img").style("float", "left").style("margin-right", "4px").attr("src", d.img).attr("width", "64px");
       }
-      tip.append("div").style("float", "left").html(dateValue );
-      tip.transition()
-      .duration(100)
-      .style("opacity", .9);
-
+      //tip.append("div").style("float", "left").html(dateValue );
+      //tip.transition()
+      //.duration(100)
+      //.style("opacity", .9);
     })
     .on("mouseout", function(){
         d3.select(this)
@@ -184,6 +188,19 @@ var TimeKnots = {
         tip.transition()
         .duration(100)
     .style("opacity", 0)});
+
+    //test ut å legge til tekst på hvert stopp
+    svg.selectAll("text")
+    .data(events).enter()
+    .append("text")
+       .text(function(d) { return d.value+" "+d.name; }).style("font-size", "100%")
+       .attr("y", function(d){
+           var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
+           return Math.floor(step*(datum - minValue) + margin)+5
+       })
+       .attr("x", function(d){
+           return Math.floor(cfg.width/2)+20
+       });
 
     //Adding start and end labels
     if(cfg.showLabels != false){
@@ -197,12 +214,12 @@ var TimeKnots = {
         var endString = maxValue;
       }
       svg.append("text")
-         .text(startString).style("font-size", "70%")
+         .text(startString).style("font-size", "100%")
          .attr("x", function(d){if(cfg.horizontalLayout){return d3.max([0, (margin-this.getBBox().width/2)])} return Math.floor(this.getBBox().width/2)})
          .attr("y", function(d){if(cfg.horizontalLayout){return Math.floor(cfg.height/2+(margin+this.getBBox().height))}return margin+this.getBBox().height/2});
 
       svg.append("text")
-         .text(endString).style("font-size", "70%")
+         .text(endString).style("font-size", "100%")
          .attr("x", function(d){if(cfg.horizontalLayout){return  cfg.width -  d3.max([this.getBBox().width, (margin+this.getBBox().width/2)])} return Math.floor(this.getBBox().width/2)})
          .attr("y", function(d){if(cfg.horizontalLayout){return Math.floor(cfg.height/2+(margin+this.getBBox().height))}return cfg.height-margin+this.getBBox().height/2})
     }
--- app/controllers/stop_map_controller.rb
+++ app/controllers/stop_map_controller.rb
@@ -4,11 +4,13 @@ layout 'stopmap'
 
   def index
     @tracks=Array.new
-    trackall = Track.all
-    trackall.each do |tn|
+    @trackall = Track.all
+    @trackall.each do |tn|
       if tn.stops.count > 0
         @tracks.concat Track.where(id: tn.id)
       end
     end
+
+
   end
 end
