007e66cabef568fe77049ddad2c05f946f4112c8
Jiska Huizing <jiska_j_huizing@hotmail.com>
2023-08-03 18:09:37 +0200

    Field guide: Changed to OSM

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,14 +4,6 @@ class LocationsController < ApplicationController
 
   def index
     @tracks = Track.sorted_by_location
-
-    @hash = Gmaps4rails.build_markers(@tracks) do |track, marker|
-      marker.lat track.latitude
-      marker.lng track.longitude
-      marker.infowindow "<input class=\"newsound\" type=\"button\" value=\"play\" data-arglink="+track.sound.to_s+" data-argtitle="+track.track_number.to_s+" />
-      Track # "+track.track_number.to_s+"</a>"+ " " + track.track_version + " // " +
-      track.location_finished + "<br>"
-    end
   end
 
 end
