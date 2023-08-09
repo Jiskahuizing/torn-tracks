b8d4108b7a9b0c159d19fda004d7e61bc1adf648
Boris Kourtoukov <boriskourt@gmail.com>
2023-08-09 19:59:42 +0200

    Update route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   get 'frontpage/index'
 
   get 'track/:id/:stop_id', :to => 'tracks#stopdelete', :as => 'delete_stop_in_track'
+
+  resources :static, except: [:new, :create, :edit, :show, :update, :destroy ]
   get 'sketch-map-track-list.json' => 'static#sketch-map-track-list', :defaults => { :format => 'json' }
 
   resources :admin_users, :except => [:show] do
